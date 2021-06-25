7c79b0991c98efded31c6665ff080b11254b956a
sander dierckx <sanderdierckx@gmail.com>
2021-06-25 15:44:23 +0200

    start a*

--- pathfinding/main.js
+++ pathfinding/main.js
@@ -3,13 +3,15 @@
  * @author sander dierckx
  * @version 1.0
  */
-
+// types [wall,start,finish,tile]
 let cellSize = 20
-let type = "wall"
+let clickType = "wall"
 let colors = { "wall": "black", "start": "green", "finish": "red" }
 let startPlaced = false
 let finishPlaced = false
 let nodes = []
+let startNode = null
+let finishNode = null
 
 function setup() {
   let can = createCanvas(windowWidth,windowHeight)
@@ -33,47 +35,107 @@ function mousePressed() {
   let getx = Math.floor(mouseX / cellSize)
   let gety = Math.floor(mouseY / cellSize)
   let cellNr = (gety * (windowHeight / cellSize)) + getx
-  if (type == "start" && startPlaced == true) {
+  if (clickType == "start" && startPlaced == true) {
     return
   }
-  if (type == "finish" && finishPlaced == true) {
+  if (clickType == "finish" && finishPlaced == true) {
     return
   }
-  let n = new node(type, cellNr, getx, gety)
+  if (clickType == "remove"){
+    nodes.forEach(no => {
+      if (no.cellNr == cellNr) {
+        if (no.type == "start") {//wanneer start verwijderen nieuwe start kunne plaatsen
+          startPlaced=false
+        }
+        if (no.type == "finish") {
+          finishPlaced=false
+        }
+        let nodeIndex = nodes.indexOf(no)// node uit array halen
+        nodes.splice(nodeIndex, 1)
+      }
+    })
+    update()
+    return
+  }
+  console.log("ja");
+  let n = new node(clickType, cellNr, getx, gety)
   n.drawCell()
   nodes.push(n)
-  if (type == "start") {
-    startPlaced=true
+  if (clickType == "start") {
+    startPlaced = true
+    n.explored = true
   }
-  if (type == "finish") {
+  if (clickType == "finish") {
     finishPlaced=true
   }
+  if (startPlaced && finishPlaced) {
+    nodes.forEach(n => {
+      if (n.type == "start") {
+        startNode == n
+      }
+      if (n.type == "finish") {
+        finishNode == n
+      }
+    })
+  }
+}
+function update() {
+  background("white")
+  drawMatrix()
+  nodes.forEach(n => {
+    n.drawCell()
+  })
 }
 
-function keyPressed(){
+function keyPressed() {
   if (keyCode === 87) {
-    type = "wall"
+    clickType = "wall"
   }
   if (keyCode === 83) {
-    type = "start"
+    clickType = "start"
   }
   if (keyCode === 70) {
-    type = "finish"
+    clickType = "finish"
+  }
+  if (keyCode === 82) {
+    clickType = "remove"
   }
 }
-
+function explore() {
+  let masterNode = null
+  nodes.forEach(n => {
+    if (n.explored) {
+      if (n.gCost > masterNode.gCost||masterNode==null) {
+        masterNode = n
+      } else {
+        continue
+      }
+    }
+  })
+}
 class node{
   constructor(type,cellNr,kol,rij){
     this.type = type
     this.cellNr = cellNr
     this.kol = kol
     this.rij = rij
-    this.color = colors[this.type]
+    this.color = colors[this.clickType]
+    this.explored = false
+    this.gCost = null
+    this.hCost = null
+    this.fCost = this.gCost+this.hCost
   }
   drawCell() {
-
     fill(this.color)
     rect(this.kol * cellSize, this.rij * cellSize, cellSize, cellSize)
-    //console.log(Boats)
+  }
+  calcCosts() {
+  //G-cost
+    if (startNode.rij == this.rij) {
+      this.gCost= abs(startNode.rij-this.rij)
+    }
+    if (startNode.kol == this.kol) {
+      this.gCost= abs(startNode.kol-this.kol)
+    }
   }
 }
